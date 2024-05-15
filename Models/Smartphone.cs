acb8e9777a9c31a91e86a1dc348200e149b02215
mdiasfran <contate.afran@gmail.com>
2024-05-15 17:39:11 -0300

    finaliza desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,17 +3,22 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo;
+        protected string Imei;
+        protected int Memoria;
 
-        public Smartphone(string numero)
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando..."); 
         }
 
         public void ReceberLigacao()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia");
+
+Smartphone nokia = new Nokia("489526352", "2280", "320098452136", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Jogo da cobrinha");
+
+Console.WriteLine("Iphone");
+
+Smartphone iphone = new Iphone("2156894265", "15", "08000215487454", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
